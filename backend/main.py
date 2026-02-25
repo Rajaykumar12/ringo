945ea4843f893679b9e3b308a64b02ea9960c45a
rajaykumar <rajaykumar12959@gmail.com>
2026-02-25 15:16:10 +0530

    routing

--- backend/main.py
+++ backend/main.py
@@ -48,7 +48,7 @@ if not os.environ.get("GROQ_API_KEY"):
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
